2651d4070ed7f253285de3fb8024a45fc5af9a0f
Mickael <mickaelramanitrera@gmail.com>
2023-06-29 17:04:42 +0300

    Handle api errors properly

--- components/contexts/swr.tsx
+++ components/contexts/swr.tsx
@@ -1,16 +1,46 @@
 "use client";
 
 import { FC, PropsWithChildren } from "react";
+import { useToast } from "@/components/ui/use-toast";
+import { useRouter } from "next/navigation";
 import { SWRConfig } from "swr";
 
-export const SWRContext: FC<PropsWithChildren<{}>> = ({ children }) => (
-  <SWRConfig
-    value={{
-      refreshInterval: 3000,
-      fetcher: (resource, init) =>
-        fetch(resource, init).then((res) => res.json()),
-    }}
-  >
-    {children}
-  </SWRConfig>
-);
+export const SWRContext: FC<PropsWithChildren<{}>> = ({ children }) => {
+  const router = useRouter();
+  const { toast } = useToast();
+
+  return (
+    <SWRConfig
+      value={{
+        refreshInterval: 3000,
+        fetcher: async (resource, init) => {
+          const res = await fetch(resource, init);
+
+          if (!res.ok) {
+            const error = new Error("An error occured fetching the datas");
+
+            (error as any).info = await res.json();
+            (error as any).status = res.status;
+
+            throw error;
+          }
+
+          return res.json();
+        },
+        onError: (error, _) => {
+          toast({
+            variant: "destructive",
+            title: "An error occurred",
+            description: "Your authentication has expired, please login again",
+          });
+
+          if (error.status === 403) {
+            router.push("/login");
+          }
+        },
+      }}
+    >
+      {children}
+    </SWRConfig>
+  );
+};
--- middleware.ts
+++ middleware.ts
@@ -2,19 +2,29 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export const config = {
-  matcher: ["/", "/dashboard/:path*"],
+  matcher: ["/", "/dashboard/:path*", "/api/:path*"],
+};
+
+const forbiddenResponse = (request: NextRequest) => {
+  const isApi = request.nextUrl.pathname.startsWith("/api");
+
+  if (isApi) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 403 });
+  }
+
+  return NextResponse.redirect(new URL("/login", request.url));
 };
 
 export const middleware = (request: NextRequest) => {
   let currentUser = request.cookies.get("user");
   if (!currentUser?.value) {
-    return NextResponse.redirect(new URL("/login", request.url));
+    return forbiddenResponse(request);
   }
 
   currentUser = JSON.parse(currentUser.value);
 
   if (!(currentUser as any)?.connected) {
-    return NextResponse.redirect(new URL("/login", request.url));
+    return forbiddenResponse(request);
   }
 
   return NextResponse.next();
