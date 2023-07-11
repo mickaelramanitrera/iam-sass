70fe9ff4b195b2eeb42b1ad898e63fac635283fe
Mickaël RAMANITRERA <mickaelramanitrera@gmail.com>
2023-07-11 18:29:46 +0300

    [TSK-14] Setup tests and ci (#2)
    * Setup tests and ci
    
    * fix action
    
    * add build action
    
    * Optimize ci
    
    * add ci on main branch merge
    
    ---------
    
    Co-authored-by: mickaelramanitrera <m.ramanitrera.ext@bloomsocialanalytics.com>

--- apps/web/components/ui/skeleton.tsx
+++ apps/web/components/ui/skeleton.tsx
@@ -1,4 +1,4 @@
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 function Skeleton({
   className,
@@ -6,10 +6,11 @@ function Skeleton({
 }: React.HTMLAttributes<HTMLDivElement>) {
   return (
     <div
+      data-testid={"skeleton"}
       className={cn("animate-pulse rounded-md bg-muted", className)}
       {...props}
     />
-  )
+  );
 }
 
-export { Skeleton }
+export { Skeleton };
