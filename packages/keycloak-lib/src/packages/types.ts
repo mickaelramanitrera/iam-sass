b17e24363b60e8eedd09e4989e5ebe9d273fe757
Mickael <mickaelramanitrera@gmail.com>
2023-07-12 18:08:43 +0300

    [TSK-17] List organizations from provider

--- packages/keycloak-lib/src/packages/types.ts
+++ packages/keycloak-lib/src/packages/types.ts
@@ -8,7 +8,7 @@ export type KeycloakResourceObject = {
   attributes?: Record<string, any>;
 };
 
-export type KeycloakResourcePayload = Omit<KeycloakResourceObject, '_id'>;
+export type KeycloakResourcePayload = Omit<KeycloakResourceObject, "_id">;
 
 export type KeycloakScopeObject = {
   id: string;
@@ -17,16 +17,20 @@ export type KeycloakScopeObject = {
   displayName: string;
 };
 
-export type KeycloakScopePayload = AtLeast<KeycloakScopeObject, 'name'>;
+export type KeycloakScopePayload = AtLeast<KeycloakScopeObject, "name">;
 
 export type KeycloakOrganizationObject = {
   id: string;
   name: string;
   attributes?: Record<string, string>;
   path: string;
+  description?: string;
 };
 
-export type KeycloakOrganizationPayload = AtLeast<KeycloakOrganizationObject, 'name'>;
+export type KeycloakOrganizationPayload = AtLeast<
+  KeycloakOrganizationObject,
+  "name"
+>;
 
 export type KeycloakUserObject = {
   id: string;
@@ -54,24 +58,27 @@ export type KeycloakUserCreationPayload = {
   enabled?: boolean;
 };
 
-export type KeycloakUserUpdatePayload = Pick<KeycloakUserCreationPayload, 'enabled'>;
+export type KeycloakUserUpdatePayload = Pick<
+  KeycloakUserCreationPayload,
+  "enabled"
+>;
 
-export type KeycloakRolePayload = Pick<KeycloakRoleObject, 'id' | 'name'>;
+export type KeycloakRolePayload = Pick<KeycloakRoleObject, "id" | "name">;
 
 export enum KeycloakLogicType {
-  POSITIVE = 'POSITIVE',
-  NEGATIVE = 'NEGATIVE',
+  POSITIVE = "POSITIVE",
+  NEGATIVE = "NEGATIVE",
 }
 
 export enum KeycloakPolicyType {
-  GROUP = 'group',
-  ROLE = 'role',
+  GROUP = "group",
+  ROLE = "role",
 }
 
 export enum KeycloakDecisionStrategyType {
-  UNANIMOUS = 'UNANIMOUS',
-  CONSENSUS = 'CONSENSUS',
-  AFFIRMATIVE = 'AFFIRMATIVE',
+  UNANIMOUS = "UNANIMOUS",
+  CONSENSUS = "CONSENSUS",
+  AFFIRMATIVE = "AFFIRMATIVE",
 }
 
 export type KeycloakBasePolicyObject = {
